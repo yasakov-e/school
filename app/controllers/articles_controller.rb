6a1641210ff1a7dbb8d3bb253aa261ba56455ce7
yasakov-e <frag199823@gmail.com>
2018-07-06 15:35:10 +0300

    Add simple views for index and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
-  def index; end
+  def index
+    @articles = Article.all
+  end
 
-  def show; end
+  def show
+    @article = Article.find(params[:id])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@
 
 Rails.application.routes.draw do
   root to: 'articles#index'
-
-  get 'articles/index'
-
-  get 'articles/show'
+  resources :articles
+  #get 'articles/index'
+  #get 'articles/show'
 end
