49bdd46513ce70aef34673679fa41878dce88c6a
yasakov-e <frag199823@gmail.com>
2018-07-09 00:56:35 +0300

    Added subjects to subjects.yml, and setup seeding in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,12 @@
 # frozen_string_literal: true
+
+require 'yaml'
+
+data = YAML.load_file('lib/fixtures/subjects.yml')
+subjects = data['subjects']
+subjects.each_pair do |name, fields|
+  Subject.create!(
+    name: name,
+    image: fields.fetch('image')
+  )
+end
