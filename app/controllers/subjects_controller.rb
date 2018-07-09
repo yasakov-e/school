c80e602459031de6c10183cab1b37d6441612e1c
yasakov-e <frag199823@gmail.com>
2018-07-09 17:31:58 +0300

    Added subjects controller and resources in routes.rb

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class SubjectsController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'subjects/index'
+
   root to: 'articles#index'
   devise_for :users
   resources :articles, only: %i[index show]
+  resources :subjects, only: %i[index]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
