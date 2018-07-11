d9ea85e35d1a07bfbbd33822c5a22c1c8798817e
yasakov-e <frag199823@gmail.com>
2018-07-11 18:43:27 +0300

    Added has_many relation to Theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Theme < ApplicationRecord
+  has_many :lessons, dependent: :destroy
   LENGTH_RANGE = 5..40
-
   validates :topic, presence: true, length: { in: LENGTH_RANGE }
 end
