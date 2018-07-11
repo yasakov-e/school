1b19a96a27840caf431b40343648ef1b5c1dfa74
yasakov-e <frag199823@gmail.com>
2018-07-11 19:31:07 +0300

    Added theme reference to lesson

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180711151443) do
+ActiveRecord::Schema.define(version: 20180711161508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20180711151443) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "theme_id"
+    t.index ["theme_id"], name: "index_lessons_on_theme_id"
   end
 
   create_table "subjects", force: :cascade do |t|
@@ -77,4 +79,5 @@ ActiveRecord::Schema.define(version: 20180711151443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lessons", "themes"
 end
