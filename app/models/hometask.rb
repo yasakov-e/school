5c0113800bdefb74ec95d04881a243d2b6c772b0
yasakov-e <frag199823@gmail.com>
2018-07-12 15:16:18 +0300

    Added validations to Homework model, rubocop fixes

--- app/models/hometask.rb
+++ app/models/hometask.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Hometask < ApplicationRecord
   belongs_to :lesson
+  validates :description, presence: true
+  validates :description, length: { minimum: 3 }
 end
