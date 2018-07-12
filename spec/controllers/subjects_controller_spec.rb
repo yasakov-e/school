e9887c170774b6d44334d49ffa71b975c57f3512
yasakov-e <frag199823@gmail.com>
2018-07-13 00:47:14 +0300

    Fixed test for subjects index response

--- spec/controllers/subjects_controller_spec.rb
+++ spec/controllers/subjects_controller_spec.rb
@@ -3,10 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe SubjectsController, type: :controller do
+  let(:user_params) do
+    {
+      name: 'sad',
+      surname: 'd',
+      email: 'foo@mail.com',
+      password: '12345678',
+      password_confirmation: '12345678',
+      approved: true
+    }
+  end
+  let(:user) { User.create(user_params) }
   describe 'GET #index' do
-    it 'returns http success' do
+    it 'returns http success if user is authenticated' do
+      login_with user
       get :index
       expect(response).to have_http_status(:success)
+    end
+    it 'returns http found if user is not authenticated' do
+      get :index
+      expect(response).to have_http_status(:found)
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'controller_helpers'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -27,6 +28,8 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include ControllerHelpers, :type => :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
