528ef7d2a44a08c7cee281590f5312168fdb4284
yasakov-e <frag199823@gmail.com>
2018-07-13 16:20:51 +0300

    Student has_many achievements relation

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Achievement < ApplicationRecord
-
+  belongs_to :user, -> { User.student }
   POINTS_RANGE = 1..12
   POINTS_LENGTH_RANGE   = 1..2
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180712120402) do
+ActiveRecord::Schema.define(version: 20180713130135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "achievements", force: :cascade do |t|
+    t.integer "points", null: false
+    t.boolean "attendance", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_achievements_on_user_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "topic", default: "", null: false
@@ -88,6 +97,7 @@ ActiveRecord::Schema.define(version: 20180712120402) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "achievements", "users"
   add_foreign_key "hometasks", "lessons"
   add_foreign_key "lessons", "themes"
 end
