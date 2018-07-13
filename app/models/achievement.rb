188d67b9a21769bb894868ffddc97ef72e8e16b4
yasakov-e <frag199823@gmail.com>
2018-07-13 15:31:05 +0300

    Added validations to Achievement model

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class Achievement < ApplicationRecord
+
+  POINTS_RANGE = 1..12
+  POINTS_LENGTH_RANGE   = 1..2
+
+  validates :points, inclusion: { in: POINTS_RANGE }
+  validates :points, length: { in: POINTS_LENGTH_RANGE }
+  validates :attendance, inclusion: { in: [true, false] }
 end
