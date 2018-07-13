0574e95a1b3c405c2862269cc97b03ccd004716e
yasakov-e <frag199823@gmail.com>
2018-07-13 15:36:07 +0300

    Added null:false and defaults to Achievement migration

--- db/migrate/20180713122444_create_achievements.rb
+++ db/migrate/20180713122444_create_achievements.rb
@@ -1,8 +1,8 @@
 class CreateAchievements < ActiveRecord::Migration[5.1]
   def change
     create_table :achievements do |t|
-      t.integer :points
-      t.boolean :attendance
+      t.integer :points, null: false
+      t.boolean :attendance, null: false, default: true
 
       t.timestamps
     end
