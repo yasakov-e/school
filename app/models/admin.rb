efb52b0f04d2d88199514746666b5680ece7215b
chkhanch <39669775+chkhanch@users.noreply.github.com>
2018-07-16 15:53:29 +0300

    Add authentification to admin panel (#33)
    * Generated Admin model by devise
    
    * Added devise routes for Admin model
    
    * Added ua localization for Admin model
    
    * Added instruction for access to site administration

---
+++ app/models/admin.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :trackable, :timeoutable, :lockable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admin, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
   root to: 'articles#index'
   devise_for :users
   resources :articles, only: %i[index show]
