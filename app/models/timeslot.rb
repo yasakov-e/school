aae522c9d43f76065ca44d566eea0e2db34b9908
yasakov-e <frag199823@gmail.com>
2018-07-16 14:30:45 +0300

    Added enum fields and validations, added unique index migration

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -2,4 +2,14 @@
 
 class Timeslot < ApplicationRecord
   belongs_to :course
+
+  DAYS = %w[monday tuesday wednesday thursday friday].freeze
+  NUMBERS = %w[1 2 3 4 5 6 7 8 9].freeze
+
+  validates :number, inclusion: { in: NUMBERS }
+  validates :day, inclusion: { in: DAYS }
+  validates :day, uniqueness: { scope: [:number, :course_id] }
+
+  enum day: DAYS
+  enum number: NUMBERS
 end
--- db/migrate/20180716104613_create_timeslots.rb
+++ db/migrate/20180716104613_create_timeslots.rb
@@ -1,8 +1,8 @@
 class CreateTimeslots < ActiveRecord::Migration[5.1]
   def change
     create_table :timeslots do |t|
-      t.integer :day
-      t.integer :number
+      t.integer :day, null: false, default: 0
+      t.integer :number, null: false, default: 1
       t.references :course, foreign_key: true
 
       t.timestamps
