77a57b4a0b2d7b83afd8c73490b1b758b4d76514
yasakov-e <frag199823@gmail.com>
2018-07-17 23:39:00 +0300

    Added query logic to index action

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class CoursesController < ApplicationController
-  def index; end
+  def index
+    if current_user.student?
+      @courses = Course.where(group_id: current_user.group_id).includes(:subject)
+    elsif current_user.teacher?
+      @courses = current_user.courses.includes(:groups)
+    end
+  end
 end
