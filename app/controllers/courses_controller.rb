daf62736ee2aea287138df812809db9abef7e67e
yasakov-e <frag199823@gmail.com>
2018-07-18 00:19:34 +0300

    Added index views for teacher and student

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,8 +4,12 @@ class CoursesController < ApplicationController
   def index
     if current_user.student?
       @courses = Course.where(group_id: current_user.group_id).includes(:subject)
+      render 'index'
     elsif current_user.teacher?
-      @courses = current_user.courses.includes(:groups)
+      @courses = current_user.courses.includes(:group)
+      render 'index_teacher'
+    else
+      redirect_to root_path
     end
   end
 end
