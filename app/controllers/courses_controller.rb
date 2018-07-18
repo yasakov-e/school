aa100cd8ca16fac78c87ac761c378f8c52d4ef3b
yasakov-e <frag199823@gmail.com>
2018-07-18 15:03:29 +0300

    Created partials for teacher and student and render them in index view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,10 +4,8 @@ class CoursesController < ApplicationController
   def index
     if current_user.student?
       @courses = Course.groups_for_user(current_user).includes(:subject)
-      render 'index'
     elsif current_user.teacher?
       @courses = current_user.courses.includes(:group)
-      render 'index_teacher'
     else
       redirect_to root_path
     end
