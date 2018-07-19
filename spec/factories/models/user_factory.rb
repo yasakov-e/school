c10d096cb1de44286fc6ed6dadab3d52ca635c26
yasakov-e <frag199823@gmail.com>
2018-07-19 13:25:47 +0300

    Added tests for timeslot validation

--- spec/factories/models/user_factory.rb
+++ spec/factories/models/user_factory.rb
@@ -5,10 +5,9 @@ FactoryBot.define do
     # Factories for valid user
     factory :valid_user do
       @pass = 'password'
-
-      email 'username@example.com'
-      name 'Username'
-      surname 'Usersurname'
+      sequence(:email) { |n| "username#{n}@example.com" }
+      sequence(:name) { |n| "username#{n}" }
+      sequence(:surname) { |n| "usersurname#{n}" }
       password @pass
 
       factory :valid_user_for_registration do
@@ -16,12 +15,12 @@ FactoryBot.define do
       end
 
       factory :valid_user_for_mentor_role do
-        email 'mentor@example.com'
+        sequence(:email) { |n| "mentor#{n}@example.com" }
         role 'mentor'
       end
 
       factory :valid_user_for_teacher_role do
-        email 'teacher@example.com'
+        sequence(:email) { |n| "teacher#{n}@example.com" }
         role 'teacher'
       end
 
