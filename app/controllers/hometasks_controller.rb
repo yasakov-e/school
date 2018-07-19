e9595125667132cd39412519de05b505c45d0425
yasakov-e <frag199823@gmail.com>
2018-07-20 00:11:59 +0300

    Added query to get last hometasks for each user course

--- app/controllers/hometasks_controller.rb
+++ app/controllers/hometasks_controller.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
 class HometasksController < ApplicationController
   def index
+    @hometasks = []
+    current_user.group.courses.each do |course|
+      @hometasks << Hometask.for_course(course).last
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
   devise_for :admin, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
   root to: 'articles#index'
   devise_for :users
