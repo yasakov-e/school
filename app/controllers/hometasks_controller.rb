66765f57c1ff5fb91ca2acb84e271d324e7460ce
yasakov-e <frag199823@gmail.com>
2018-07-20 02:40:17 +0300

    Added tests for hometask index action

--- app/controllers/hometasks_controller.rb
+++ app/controllers/hometasks_controller.rb
@@ -2,9 +2,13 @@
 
 class HometasksController < ApplicationController
   def index
-    @hometasks = []
-    current_user.group.courses.each do |course|
-      @hometasks << Hometask.for_course(course).last
+    if current_user.student?
+      @hometasks = []
+      current_user.group.courses.each do |course|
+        @hometasks << Hometask.for_course(course).last
+      end
+    else
+      redirect_to root_path
     end
   end
 end
--- spec/controllers/hometasks_controller_spec.rb
+++ spec/controllers/hometasks_controller_spec.rb
@@ -3,10 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe HometasksController, type: :controller do
+  # Helpers definition
+  let(:other_role) { FactoryBot.create(:valid_user_for_mentor_role) }
+
+  let(:student) { FactoryBot.create(:valid_user_with_group) }
+  let(:teacher) { course.user }
+
   describe 'GET #index' do
-    it 'returns http success' do
+    it 'returns http success if user is a student' do
+      login_with student
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it 'redirects to root if user has other role' do
+      login_with other_role
+      get :index
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to root_path
     end
   end
 end
