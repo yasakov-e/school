8ff25839a682970b6d06706cf8fcf20c75cce8ed
chkhanch <39669775+chkhanch@users.noreply.github.com>
2018-07-24 16:56:11 +0300

    Bug timeslots (#60)
    * Added new tests for Timeslots controller
    
    * Fixed BUG at Timeslots controller

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -3,17 +3,38 @@
 class TimeslotsController < ApplicationController
   def index
     empty_timetable
-    current_user.group.courses.each do |course|
-      course.timeslots.each do |timeslot|
-        @timetable[timeslot.day.to_sym].push(
-          subject: course.subject,
-          hometask: Hometask.last || 'no hometask'
-        )
-      end
+    if current_user.student?
+      student_timetable
+    elsif current_user.teacher? && current_user.courses.first
+      teacher_timetable
+    else
+      redirect_to root_path
     end
   end
 
   private
+
+  def student_timetable
+    current_user.group.courses.each do |course|
+      course.timeslots.each do |timeslot|
+        @timetable[timeslot.day.to_sym][timeslot.number.to_i] = {
+          subject: course.subject,
+          hometask: Hometask.last || { description: 'no hometask' }
+        }
+      end
+    end
+  end
+
+  def teacher_timetable
+    current_user.courses.each do |course|
+      course.timeslots.each do |timeslot|
+        @timetable[timeslot.day.to_sym][timeslot.number.to_i] = {
+          group: course.group,
+          hometask: Hometask.last || { description: 'no hometask' }
+        }
+      end
+    end
+  end
 
   def empty_timetable
     @timetable = Timeslot::DAYS.each_with_object({}) do |day, hash|
--- spec/controllers/timeslots_controller_spec.rb
+++ spec/controllers/timeslots_controller_spec.rb
@@ -5,10 +5,31 @@ require 'rails_helper'
 RSpec.describe TimeslotsController, type: :controller do
   let(:timeslot) { FactoryBot.create(:valid_timeslot) }
   let(:student) { FactoryBot.create(:valid_user_for_registration, group_id: timeslot.group_id) }
+  let(:teacher) { student.group.courses.first.user }
+  let(:teacher_without_course) { FactoryBot.create(:valid_user_for_teacher_role) }
+  let(:mentor) { FactoryBot.create(:valid_user_for_mentor_role) }
 
   it 'returns http success if user is a student' do
     login_with student
     get :index
     expect(response).to have_http_status(:success)
+  end
+
+  it 'returns http success if user is a teacher with course' do
+    login_with teacher
+    get :index
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'returns http success if user is a teacher with course' do
+    login_with teacher_without_course
+    get :index
+    expect(response).to redirect_to root_path
+  end
+
+  it 'redirect if user is a mentor' do
+    login_with mentor
+    get :index
+    expect(response).to redirect_to root_path
   end
 end
